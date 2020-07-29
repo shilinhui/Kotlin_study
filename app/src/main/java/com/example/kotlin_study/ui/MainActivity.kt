4f6a1bc0dde04d2f3c06888b461ead06f7831c2f
shilh4 <shilh4@lenovo.com>
2020-07-29 16:25:47 +0800

    git commit

--- app/src/main/java/com/example/kotlin_study/ui/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_study/ui/MainActivity.kt
@@ -19,7 +19,7 @@ import com.example.kotlin_study.ui.fragment.IOSFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
- * git 提交测试指定文件
+ * git 提交测试指定文件Commit
  */
 class MainActivity : BaseBindingActivity<ActivityMainBinding>() {
 
